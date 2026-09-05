96804cfe4610abca995819370d2c93bc94666776
JuicySwagUser <JuicySwagUser@DESKTOP-CM441TC>
2026-09-05 09:23:00 +0300

    Добавьте файлы проекта.

---
+++ git_test_1/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        int a = int.Parse(Console.ReadLine());
+        int b = int.Parse(Console.ReadLine());
+        int c = a + b;
+        Console.WriteLine(c);
+    }
+}
